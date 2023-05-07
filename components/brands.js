495e8a06ecacc0fddd3ceaa8e77204ca58d88572
Mufaqar <mufaqar@gmail.com>
2023-05-07 18:53:13 +0500

    Testimonails

--- components/brands.js
+++ components/brands.js
@@ -1,75 +1,108 @@
-import React from 'react'
-import Image from 'next/image'
-import brand1 from '../public/images/brands/brands1.png'
-import brand2 from '../public/images/brands/brands2.png'
-import brand3 from '../public/images/brands/brands3.png'
-import brand6 from '../public/images/brands/brands6.png'
-import brand5 from '../public/images/brands/brands5.png'
+import React from 'react';
+import Image from 'next/image';
+import brand1 from '../public/images/brands/brands1.png';
+import brand2 from '../public/images/brands/brands2.png';
+import brand3 from '../public/images/brands/brands3.png';
+import brand6 from '../public/images/brands/brands6.png';
+import brand5 from '../public/images/brands/brands5.png';
 import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-hook-inview'
+import { useInView } from 'react-hook-inview';
 import { useEffect } from 'react';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, Navigation } from 'swiper';
 
 export const Brands = () => {
+  const [ref, inView] = useInView();
+  const animationOpacity = useAnimation();
 
-    const [ref, inView] = useInView();
-    const animationOpacity = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      animationOpacity.start({
+        opacity: 1,
+        scale: 1,
+        transition: {
+          type: 'spring',
+          duration: 2,
+          bounce: 0.3,
+        },
+      });
+    } else {
+      animationOpacity.start({
+        opacity: 0,
+        scale: 0.8,
+      });
+    }
+  }, [inView]);
 
-    useEffect(() => {
-        if (inView) {
-            animationOpacity.start({
-                opacity: 1,
-                scale: 1,
-                transition: {
-                    type: "spring", duration: 2, bounce: 0.3
-                }
-            })
-        } else {
-
-            animationOpacity.start({
-                opacity: 0,
-                scale: 0.8
-            })
-        }
-    }, [inView]);
-
-    return (
-        <div>
-            <h2 className="after:content-['.'] after:ml-0.5 after:text-customPink md:text-6xl text-4xl leading-7 font-bold tracking-wide text-center font-Montserrat">
-                Clients
-            </h2>
-            <p className="max-w-2xl mx-auto mt-4 text-lg leading-7 tracking-wide text-center text-gray-400">
-                Uniquely promote adaptive quality vectors rather than stand-alone
-                e-markets. pontificate alternative architectures whereas iterate.
-            </p>
-            <div ref={ref} className=''>
-                <Swiper
-                    // install Swiper modules
-                    modules={[Navigation, Autoplay]}
-                    loop={true}
-                    autoplay={{
-                        delay: 1500,
-                    }}
-                    slidesPerView={5}
-                    spaceBetween={10}
-                    navigation={false}
-                >
-
-                    <SwiperSlide><motion.figure animate={animationOpacity} className='max-w-[12rem] mx-auto grayscale hover:grayscale-0'><Image src={brand1} alt="Brands Picture" /></motion.figure></SwiperSlide>
-                    <SwiperSlide><motion.figure animate={animationOpacity} className='max-w-[12rem] mx-auto grayscale hover:grayscale-0'><Image src={brand2} alt="Brands Picture" /></motion.figure></SwiperSlide>
-                    <SwiperSlide><motion.figure animate={animationOpacity} className='max-w-[12rem] mx-auto grayscale hover:grayscale-0'><Image src={brand3} alt="Brands Picture" /></motion.figure></SwiperSlide>
-                    <SwiperSlide><motion.figure animate={animationOpacity} className='max-w-[12rem] mx-auto grayscale hover:grayscale-0'><Image src={brand5} alt="Brands Picture" /></motion.figure></SwiperSlide>
-                    <SwiperSlide><motion.figure animate={animationOpacity} className='max-w-[12rem] mx-auto grayscale hover:grayscale-0'><Image src={brand6} alt="Brands Picture" /></motion.figure></SwiperSlide>
-                </Swiper>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2 className="after:content-['.'] after:ml-0.5 after:text-customPink md:text-6xl text-4xl leading-7 font-bold tracking-wide text-center font-Montserrat">
+        Clients
+      </h2>
+      <p className="max-w-2xl mx-auto mt-4 text-lg leading-7 tracking-wide text-center text-gray-400">
+        Our past clients have been happy and satisfied with our work.
+      </p>
+      <div ref={ref} className="">
+        <Swiper
+          // install Swiper modules
+          modules={[Navigation, Autoplay]}
+          loop={true}
+          autoplay={{
+            delay: 1500,
+          }}
+          slidesPerView={5}
+          spaceBetween={10}
+          navigation={false}
+        >
+          <SwiperSlide>
+            <motion.figure
+              animate={animationOpacity}
+              className="max-w-[12rem] mx-auto grayscale hover:grayscale-0"
+            >
+              <Image src={brand1} alt="Brands Picture" />
+            </motion.figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <motion.figure
+              animate={animationOpacity}
+              className="max-w-[12rem] mx-auto grayscale hover:grayscale-0"
+            >
+              <Image src={brand2} alt="Brands Picture" />
+            </motion.figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <motion.figure
+              animate={animationOpacity}
+              className="max-w-[12rem] mx-auto grayscale hover:grayscale-0"
+            >
+              <Image src={brand3} alt="Brands Picture" />
+            </motion.figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <motion.figure
+              animate={animationOpacity}
+              className="max-w-[12rem] mx-auto grayscale hover:grayscale-0"
+            >
+              <Image src={brand5} alt="Brands Picture" />
+            </motion.figure>
+          </SwiperSlide>
+          <SwiperSlide>
+            <motion.figure
+              animate={animationOpacity}
+              className="max-w-[12rem] mx-auto grayscale hover:grayscale-0"
+            >
+              <Image src={brand6} alt="Brands Picture" />
+            </motion.figure>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
--- components/testimonial.js
+++ components/testimonial.js
@@ -1,63 +1,75 @@
-import Image from "next/image";
+import Image from 'next/image';
 import Image_url from '../public/images/dp.jpg';
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
+import { Autoplay, Pagination, Navigation } from 'swiper';
 
 export default function Testimonial() {
-    return (
-        <>
-            <div className='md:max-w-[1110px] mx-auto flex md:flex-row flex-col'>
-                <div className='md:w-1/4 w-full bg-customPink py-14 px-8 items-center'>
-                    <div>
-                        <h5 className="text-xl font-bold tracking-tighter text-white mb-2">Our Happy Clients</h5>
-                        <p className='text-white font-medium text-base'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt nulla tortor,</p>
-                    </div>
-                </div>
-                <div className='md:w-3/4 w-full p-7 bg-gray-100'>
-                    <Swiper
-                        // install Swiper modules
-                        modules={[Navigation, Autoplay]}
-                        loop={true}
-                        autoplay={{
-                            delay: 2000,
-                        }}
-                        slidesPerView={1}
-                        navigation={true}
-                    >
-                        <SwiperSlide>{slide()}</SwiperSlide>
-                    </Swiper>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="md:max-w-[1110px] mx-auto flex md:flex-row flex-col">
+        <div className="md:w-1/4 w-full bg-customPink py-14 px-8 items-center">
+          <div>
+            <h5 className="text-xl font-bold tracking-tighter text-white mb-2">
+              Our Happy Clients
+            </h5>
+            <p className="text-white font-medium text-base">
+              Our top priority is satisfied clients. We deliver high-quality
+              work and personalized service to earn trust and loyalty.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-3/4 w-full p-7 bg-gray-100">
+          <Swiper
+            // install Swiper modules
+            modules={[Navigation, Autoplay]}
+            loop={true}
+            autoplay={{
+              delay: 2000,
+            }}
+            slidesPerView={1}
+            navigation={true}
+          >
+            <SwiperSlide>{slide()}</SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
 }
 
 function slide() {
-    return (
-        <div className="bg-white p-7">
-            <p className='text-base text-slate-700 mb-7'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat vitae dui vitae vehicula. Quisque in tincidunt ligula, nec dignissim arcu.</p>
-            <p className='text-base text-slate-700 mb-7'>Etiam tincidunt nulla tortor, a imperdiet enim tristique nec. Nulla lobortis leo eget metus dapibus sodales. Sed placerat vitae dui vitae vehicula. Quisque in tincidunt ligula, nec dignissim arcu.</p>
-            <div className="flex items-center space-x-7">
-                <div className="relative w-12  h-12 rounded-full">
-                    <Image
-                        src={Image_url}
-                        alt=""
-                        className="rounded-full h-full w-full"
-                    />
-                </div>
-                <div>
-                    <h5 className="font-bold">John Doe</h5>
-                    <p className="font-normal">Aple Inc</p>
-                </div>
-
-            </div>
+  return (
+    <div className="bg-white p-7">
+      <p className="text-base text-slate-700 mb-7">
+        Mufaqar did an outstanding job with our website redesign. They were
+        responsive, detail-oriented, and delivered a website that exceeded our
+        expectations
+      </p>
+      <p className="text-base text-slate-700 mb-7">
+        We had a tight deadline for our website project, but Mufaqar and his
+        team delivered on time and on budget. They were professional and easy to
+        work with every step of the way."
+      </p>
+      <div className="flex items-center space-x-7">
+        <div className="relative w-12  h-12 rounded-full">
+          <Image
+            src={Image_url}
+            alt=""
+            className="rounded-full h-full w-full"
+          />
         </div>
-    )
+        <div>
+          <h5 className="font-bold">Emma Watson</h5>
+          <p className="font-normal"> PowerWashing</p>
+        </div>
+      </div>
+    </div>
+  );
 }
