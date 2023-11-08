a3ec6cac199546e348de52fa02464a918e82a4b1
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-11-08 19:38:11 +0500

    api

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,38 +1,44 @@
-import { useFormik } from 'formik';
 import { BsArrowRight, BsBuilding, BsEnvelope, BsTelephone } from 'react-icons/bs';
 import InnerPages_Banner from '../components/innerPages-banner';
-import * as Yup from 'yup';
 import Link from 'next/link';
 import { motion, Variant } from 'framer-motion'
 import { fadeUp } from "../const/animation";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from 'react';
 
-
-export const signUpschema = Yup.object({
-
-    name: Yup.string().required("Please select field"),
-    email: Yup.string().email().required("Please select field"),
-    phone: Yup.string().required("Please select field"),
-    textarea: Yup.string().required("Please select field"),
-});
 
 export default function Contact_Us() {
 
-    const initialValues = {
-        name: '',
-        email: '',
-        phone: '',
-        textarea: '',
-    }
-    const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
-        initialValues,
-        validationSchema: signUpschema,
-        onSubmit: (values) => {
-            console.log(values)
-        }
-    })
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm();
 
+    const [sending, setSending] = useState(false)
+
+    const onSubmit = (data) => {
+        setSending(true)
+        fetch('/api/email', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        }).then((res) => {
+            console.log('Response received', res);
+            if (res.status === 200) {
+                console.log('Response succeeded!');
+                alert('Message Successfully send.!');
+                reset();
+                setSending(false)
+            }
+        });
+    }
     return (
-        <>
+        <main className='overflow-hidden'>
             <InnerPages_Banner sub="I’d love to plan, design & develop your websit" title="LET’S WORK TOGETHER" />
 
             <motion.section
@@ -100,46 +106,53 @@ export default function Contact_Us() {
                     </motion.h3>
                     <motion.form
                         variants={fadeUp}
-                        onSubmit={handleSubmit} action="#" className='md:w-3/4 w-full mx-auto'>
-                        <div className='flex md:flex-row flex-col mb-4'>
-                            <label htmlFor='name' className='hidden'>Your Name</label>
-                            <input
-                                type="text"
-                                name="name"
-                                placeholder="Your Name"
-                                value={values.name}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className="md:w-1/2 w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm uppercase md:mr-4 md:mb-0 mb-4" />
+                        onSubmit={handleSubmit(onSubmit)} className='md:w-3/4 w-full mx-auto'>
+                        <div className='flex md:flex-row flex-col gap-4 mb-4'>
+                            <div className='md:w-1/2 w-full'>
+                                <label htmlFor='name' className='hidden'>Your Name</label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    placeholder="Your Name"
+                                    className="w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm md:mr-4 md:mb-0 mb-4"
+                                    {...register("name", { required: true })}
+                                />
+                                {errors.name && <span className='text-xs text-red-500'>This field is required</span>}
 
-                            <label htmlFor='email' className='hidden'>Your Email</label>
-                            <input
-                                type="email"
-                                name="email"
-                                placeholder="Your Email"
-                                value={values.email}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className="md:w-1/2 w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm uppercase" />
+                            </div>
+                            <div className='md:w-1/2 w-full'>
+                                <label htmlFor='email' className='hidden'>Your Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    placeholder="Your Email"
+                                    className="w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm"
+                                    {...register("email", { required: true })}
+                                />
+                                {errors.email && <span className='text-xs text-red-500'>This field is required</span>}
+                            </div>
                         </div>
-                        <label htmlFor='phone' className='hidden'>Your Phone</label>
-                        <input
-                            type="tel"
-                            name="phone"
-                            placeholder="Your Phone"
-                            value={values.phone}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            className="w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm uppercase mb-4" />
-                        <textarea
-                            type="text"
-                            name="textarea"
-                            rows="10"
-                            placeholder="Tell us a little bit about your project"
-                            value={values.textarea}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            className="w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm uppercase mb-4"></textarea>
+                        <div className='w-full'>
+                            <label htmlFor='phone' className='hidden'>Your Phone</label>
+                            <input
+                                type="phone"
+                                id="phone"
+                                placeholder="Your Phone"
+                                className="w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm mb-4"
+                                {...register("phone", { required: true })}
+                            />
+                            {errors.phone && <span className='text-xs text-red-500'>This field is required</span>}
+                        </div>
+                        <div className='w-full'>
+                            <textarea
+                                type="text"
+                                id="message"
+                                rows="10"
+                                placeholder="Tell us a little bit about your project"
+                                className="w-full border border-gray-300 focus:border-customPink outline-none py-4 px-6 text-sm mb-4"
+                                {...register("message", { required: true })}
+                            ></textarea>
+                        </div>
                         <button
                             type='submit'
                             className='border-2 border-customPink flex w-44 h-14 justify-between items-center text-sm uppercase text-customPink font-medium p-4 hover:bg-customPink hover:w-48 hover:text-white transform transition-all duration-300'>
@@ -148,6 +161,6 @@ export default function Contact_Us() {
                     </motion.form>
                 </div>
             </motion.section>
-        </>
+        </main>
     )
 }
