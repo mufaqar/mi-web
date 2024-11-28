bf10ca4c5cffa14f6e09aa8fda617ce11cfd57c8
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-11-28 14:37:14 +0500

    CV nab

--- components/cv-fixNav.js
+++ components/cv-fixNav.js
@@ -11,8 +11,8 @@ export default function CV_fixNav() {
                 <ul>
                     <li >
                         <Link href="#about">
-                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group"><AiOutlineHome />
-                            <div className="shadow-2xl  w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block duration-700">
+                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group transition-all duration-300"><AiOutlineHome />
+                            <div className="shadow-2xl  w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block">
                                 <p className="relative before:content-[''] before:w-4 before:h-4 before:bg-bs_secondary before:absolute before:-left-2 before:top-5 before:transform before:rotate-45
                                 text-xs leading-[2rem] text-center font-bold text-title_color py-3 px-4 uppercase">About me</p>
                             </div>
@@ -21,8 +21,8 @@ export default function CV_fixNav() {
                     </li>
                     <li >
                         <Link href="#experience">
-                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group"><AiOutlineEye />
-                            <div className="shadow-2xl w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block duration-700">
+                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group transition-all duration-300"><AiOutlineEye />
+                            <div className="shadow-2xl w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block">
                                 <p className="relative before:content-[''] before:w-5 before:h-5 before:bg-bs_secondary before:absolute before:-left-2 before:top-5 before:transform before:rotate-45
                                 text-xs leading-[2rem] text-center font-bold text-title_color py-3 px-4 uppercase">Experience</p>
                             </div>
@@ -31,8 +31,8 @@ export default function CV_fixNav() {
                     </li>
                     <li >
                         <Link href="#education">
-                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group"><BiBriefcase />
-                            <div className="shadow-2xl w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block duration-700">
+                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group transition-all duration-300"><BiBriefcase />
+                            <div className="shadow-2xl w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block">
                                 <p className="relative before:content-[''] before:w-5 before:h-5 before:bg-bs_secondary before:absolute before:-left-2 before:top-5 before:transform before:rotate-45
                                 text-xs leading-[2rem] text-center font-bold text-title_color py-3 px-4 uppercase">Education</p>
                             </div>
@@ -41,8 +41,8 @@ export default function CV_fixNav() {
                     </li>
                     <li >
                         <Link href="#skills">
-                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group"><GiGraduateCap />
-                            <div className="shadow-2xl w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block duration-700">
+                            <div className="text-white text-4xl p-3 bg-title_color hover:bg-bs_secondary hover:text-title_color flex items-center group transition-all duration-300"><GiGraduateCap />
+                            <div className="shadow-2xl w-32 h-14 absolute bg-bs_secondary left-[4.5rem] hidden group-hover:block">
                                 <p className="relative before:content-[''] before:w-5 before:h-5 before:bg-bs_secondary before:absolute before:-left-2 before:top-5 before:transform before:rotate-45
                                 text-xs leading-[2rem] text-center font-bold text-title_color py-3 px-4 uppercase">Skills</p>
                             </div>
